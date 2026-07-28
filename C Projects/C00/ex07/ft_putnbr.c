6441685971c1661ab88b45b1740dd51e5acae398
mauditdestin <akapaui@protonmail.com>
2026-07-28 17:46:37 +0200

    + | added putnbr

--- C Projects/C00/ex07/ft_putnbr.c
+++ C Projects/C00/ex07/ft_putnbr.c
@@ -6,6 +6,46 @@
 /*   By: username <username@student.42tokyo.jp>    #+#  +:+       +#+         */
 /*                                               +#+#+#+#+#+   +#+            */
 /*   Created: 2026/07/28 05:22:16 by username         #+#    #+#              */
-/*   Updated: 2026/07/28 05:22:17 by username        ###   ########.fr        */
+/*   Updated: 2026/07/28 17:46:24 by username        ###   ########.fr        */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_putnbr(int nb)
+{
+	int	n;
+
+	n = nb;
+	if (n == -2147483648)
+		write(1, "-2147483648", 11);
+	if (n < 0)
+	{
+		ft_putchar('-');
+		n *= -1;
+	}
+	if (n >= 0 && n <= 9)
+		ft_putchar(n + '0');
+	if (n > 9)
+	{
+		ft_putnbr(n / 10);
+		ft_putnbr(n % 10);
+	}
+}
+
+// int	main(void)
+// {
+// 	ft_putnbr(-2147483648);
+// 	write(1, "\n", 1);
+// 	ft_putnbr(-214);
+// 	write(1, "\n", 1);
+// 	ft_putnbr(123);
+// 	write(1, "\n", 1);
+// 	ft_putnbr(0);
+// 	write(1, "\n", 1);
+// }

80d1dbc3fdfd08da6925fc13337469ca7240cf42
mauditdestin <akapaui@protonmail.com>
2026-07-28 21:34:25 +0200

    + | Added ft_rev_int_tab and started ft_sort_int_tab

--- C Projects/C01/ex07/ft_rev_int_tab.c
+++ C Projects/C01/ex07/ft_rev_int_tab.c
@@ -1,15 +1,45 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                       :::      ::::::::    */
-/*   ft_rev_in_tab.c                                   :+:      :+:    :+:    */
+/*   ft_rev_int_tab.c                                  :+:      :+:    :+:    */
 /*                                                   +:+ +:+         +:+      */
 /*   By: username <username@student.42tokyo.jp>    #+#  +:+       +#+         */
 /*                                               +#+#+#+#+#+   +#+            */
 /*   Created: 2026/07/28 18:12:39 by username         #+#    #+#              */
-/*   Updated: 2026/07/28 18:37:33 by username        ###   ########.fr        */
+/*   Updated: 2026/07/28 21:33:46 by username        ###   ########.fr        */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdio.h>
+
 void	ft_rev_int_tab(int *tab, int size)
 {
+	int	i;
+	int	temp;
+
+	i = 0;
+	while (i < size / 2)
+	{
+		temp = tab[i];
+		tab[i] = tab[(size - 1) - i];
+		tab[(size - 1) - i] = temp;
+		i++;
+	}
 }
+
+// int	main(void)
+// {
+// 	int	tab[5] =
+// 	{
+// 		1, 2, 3, 4, 5
+// 	};
+// 	int	i = 0;
+
+// 	ft_rev_int_tab(tab, 5);
+// 	while (i < 5)
+// 	{
+// 		printf("%d\n", tab[i]);
+// 		i++;
+// 	}
+// 	return (0);
+// }

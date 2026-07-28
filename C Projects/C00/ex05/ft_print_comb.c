b752079a57aa521e9da0c15f563dce44321f0297
mauditdestin <akapaui@protonmail.com>
2026-07-28 17:39:16 +0200

    + | Added ex05 ex06

--- C Projects/C00/ex05/ft_print_comb.c
+++ C Projects/C00/ex05/ft_print_comb.c
@@ -6,22 +6,47 @@
 /*   By: username <username@student.42tokyo.jp>    #+#  +:+       +#+         */
 /*                                               +#+#+#+#+#+   +#+            */
 /*   Created: 2026/07/28 05:20:02 by username         #+#    #+#              */
-/*   Updated: 2026/07/28 05:21:30 by username        ###   ########.fr        */
+/*   Updated: 2026/07/28 17:34:45 by username        ###   ########.fr        */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <unistd.h>
+
+void	ft_print_vals(char first, char second, char third)
+{
+	write(1, &first, 1);
+	write(1, &second, 1);
+	write(1, &third, 1);
+	if (!(first == '7' && second == '8' && third == '9'))
+		write(1, ", ", 2);
+}
+
 void	ft_print_comb(void)
 {
-	int	a;
-	int	b;
-	int	c;
+	char	a;
+	char	b;
+	char	c;
 
-	a = 0;
-	while ()
+	a = '0';
+	while (a <= '7')
+	{
+		b = a + 1;
+		while (b <= '8')
+		{
+			c = b + 1;
+			while (c <= '9')
+			{
+				ft_print_vals(a, b, c);
+				c++;
+			}
+			b++;
+		}
+		a++;
 	}
-
-int	main(void)
-{
-	ft_print_comb();
-	return (0);
 }
+
+// int	main(void)
+// {
+// 	ft_print_comb();
+// 	return (0);
+// }
--- C Projects/C00/ex06/ft_print_comb2.c
+++ C Projects/C00/ex06/ft_print_comb2.c
@@ -6,6 +6,43 @@
 /*   By: username <username@student.42tokyo.jp>    #+#  +:+       +#+         */
 /*                                               +#+#+#+#+#+   +#+            */
 /*   Created: 2026/07/28 05:22:11 by username         #+#    #+#              */
-/*   Updated: 2026/07/28 05:22:11 by username        ###   ########.fr        */
+/*   Updated: 2026/07/28 17:39:02 by username        ###   ########.fr        */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_print_comb2(void)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	while (i <= 99)
+	{
+		j = i + 1;
+		while (j <= 99)
+		{
+			ft_putchar(i / 10 + '0');
+			ft_putchar(i % 10 + '0');
+			ft_putchar(' ');
+			ft_putchar(j / 10 + '0');
+			ft_putchar(j % 10 + '0');
+			if (!(i == 98 && j == 99))
+				write(1, ", ", 2);
+			j++;
+		}
+		i++;
+	}
+}
+
+// int	main(void)
+// {
+// 	ft_print_comb2();
+// 	return (0);
+// }

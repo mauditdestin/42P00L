d507e52dc41b9364615170c55aaf43564c108727
mauditdestin <akapaui@protonmail.com>
2026-07-28 18:00:15 +0200

    + | Added ex03

---
+++ C Projects/C01/ex03/ft_div_mod.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                       :::      ::::::::    */
+/*   ft_div_mod.c                                      :+:      :+:    :+:    */
+/*                                                   +:+ +:+         +:+      */
+/*   By: username <username@student.42tokyo.jp>    #+#  +:+       +#+         */
+/*                                               +#+#+#+#+#+   +#+            */
+/*   Created: 2026/07/28 17:59:22 by username         #+#    #+#              */
+/*   Updated: 2026/07/28 18:00:07 by username        ###   ########.fr        */
+/*                                                                            */
+/* ************************************************************************** */
+
+void	ft_div_mod(int a, int b, int *div, int *mod)
+{
+	*div = a / b;
+	*mod = a % 10;
+}
+
+// int	main(void)
+// {
+// 	int	a = 45;
+// 	int	b = 5;
+// 	int	v1 = 0;
+// 	int	v2 = 0;
+// 	int	*div = &v1;
+// 	int	*mod = &v2;
+
+// 	printf("Before ft_div_mod: \n");
+// 	printf("Pointer 'div' points to adress %p that has the value %d stored\n", div, *div);
+// 	printf("Pointer 'mod' points to adress %p that has the value %d stored\n", mod, *mod);
+// 	ft_div_mod(a, b, div, mod);
+// 	printf("\n");
+// 	printf("After ft_div_mod: \n");
+// 	printf("Pointer 'div' remains pointing to adress %p, but now that has the value %d stored\n", div, *div);
+// 	printf("Pointer 'mod' remains pointing to adress %p, but now that has the value %d stored\n", mod, *mod);
+// }
